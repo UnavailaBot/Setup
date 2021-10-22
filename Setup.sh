9cbf9f104826d5002912f63f2de2722ca8f7ee1d
UnavailaBot <89625749+UnavailaBot@users.noreply.github.com>
2021-10-22 13:28:27 -0600

    Update Setup.sh

--- Setup.sh
+++ Setup.sh
@@ -23,6 +23,11 @@ sudo python3 -m install warble
 python3      -m pip install metawear
 sudo python3 -m pip install metawear
 
+curl -fsSL https://cli.github.com/packages/githubcli-archive-keyring.gpg | sudo gpg --dearmor -o /usr/share/keyrings/githubcli-archive-keyring.gpg
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | sudo tee /etc/apt/sources.list.d/github-cli.list > /dev/null
+sudo apt update
+sudo apt install gh
+
 sudo apt autoremove -y
 sudo apt autoclean  -y
 
